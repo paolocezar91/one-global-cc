e0d90d3844ebf0eedd5970ca572022a6da132647
Paolo <pcezar87@gmail.com>
2025-08-23 11:06:37 -0300

    Linting

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import { UserList } from "@/components/user-list/user-list";
+import Link from "next/link";
 
 export default function Dashboard() {
   return <div className="flex flex-col">
     <div className="text-right">
-      <a href="/user/create" className="hover:underline">Create</a>
+      <Link href="/user/create" className="hover:underline">Create</Link>
     </div>
     <UserList />
   </div>;
